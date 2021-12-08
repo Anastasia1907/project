a7ec124755f8b4a13fee11e3e5e0a6895721f0aa
Anastasia <brokennote@yandex.ru>
2021-12-08 16:53:54 +0200

    update_0812

--- cypress/integration/toys.spec.js
+++ cypress/integration/toys.spec.js
@@ -1,34 +1,56 @@
 /// <reference types="cypress" />
 
+const { toString } = require("lodash")
+
 Cypress.on('uncaught:exception', (err, runnable) => {
     return false
 })
 
+const cardInfo = []
+
 const URL = 'http://demo.guru99.com/payment-gateway/index.php'
 
+const arr = []
 
 describe('test toy shop', () => {
     beforeEach(() => {
-        cy.visit(URL)
+        cy.visit('http://demo.guru99.com/payment-gateway/cardnumber.php')
     })
     it('credit card creation', () => {
-        cy.visit('http://demo.guru99.com/payment-gateway/cardnumber.php')
-        cy.get('#nav > a:nth-child(2)').click()
-        cy.get('#three > div > header > h2').should('contain', 'Here is your New Card')
-        let cardNumber, CVV, limit
-        cy.get('#three > div > h4:nth-child(3)').then(function (el) {
-            cardNumber = el.text()
-            cy.log(cardNumber)
-            let numcard
-            numcard = 
-            cy.log(numcard)
-            cy.get('#nav > [href="check_credit_balance.php"]').click()
-            cy.get('h2').should('contain', 'Check Credit Balance')
-            cy.get('#card_nmuber').type(numcard)
+        cy.get('#three > div').children('h4').then((els) => {
+            let arrEls = []
+            els.each((i, el) => {
+                let strEl = el.textContent;
+                if(strEl.includes('Exp:-')) {
+                    strEl = strEl.replace("Exp:- ", "")
+                    strEl = strEl.split('/')
+                    strEl[0] = strEl[0].replace("0", "")
+                    strEl[0] = strEl[0].toString()
+                    strEl[0] = strEl[0].replace(" ", "")
+                    cardInfo.push(strEl)
+                }
+                else {
+                arrEls.push(strEl)
+                }
+            })
+            arrEls.forEach(el => {
+                if(el.includes(Array)) {
+                    cardInfo.push(el)
+                }
+                else {
+                el = parseInt(el.match(/\d+/))
+                cardInfo.push(el)
+                }
+            })
+            
+           cy.get('#header > div > a.logo').click()
             cy.get('.button').click()
-            cy.get('[size="6.2em;"]').should('contain', 'This Card Not Any Transactions')
+            cy.get('#card_nmuber').type(cardInfo[1])
+            cy.get('#month').select(cardInfo[0][0])
+            cy.get('#year').select(cardInfo[0][1])
+            cy.get('#cvv_code').type(cardInfo[2])
+            cy.get('.button').click()
+            cy.get('#three > div > div > h2').should('contain', 'Payment successfull!')
         })
-        
-        cy.log(cardNumber)
     })
 })
